27a3c8f3f75f6018642bd3b571d9fe93f32164b9
wukangjie <897288110@qq.com>
2020-03-28 23:33:11 +0800

    1.0版本提交准备发布啦

---
+++ mvnautoreleaseapi/src/main/java/mvn/auto/release/api/ITestService.java
@@ -0,0 +1,9 @@
+package mvn.auto.release.api;
+
+/**
+ * @author luolanjiao
+ * @date 2020/3/28
+ */
+public interface ITestService {
+    void test();
+}
---
+++ mvnautoreleaseservice/src/main/java/mvn.auto.release.service/TestServiceImpl.java
@@ -0,0 +1,16 @@
+package mvn.auto.release.service;
+
+import mvn.auto.release.api.ITestService;
+
+/**
+ * @author luolanjiao
+ * @date 2020/3/28
+ *
+ *
+ */
+public class TestServiceImpl implements ITestService {
+    @Override
+    public void test() {
+
+    }
+}
