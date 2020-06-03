82eb48fd11536352a1dc08a73e9917b4de4c689a
wukangjie <897288110@qq.com>
2020-06-03 22:55:34 +0800

    ddd

---
+++ mvnautoreleaseservice/src/main/java/mvn.auto.release.service/Test.java
@@ -0,0 +1,13 @@
+package mvn.auto.release.service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author luolanjiao
+ * @date 2020/5/19
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(String.format("abc%s%s", "d", "e"));
+    }
+}
